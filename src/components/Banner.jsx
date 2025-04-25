dbe13b196f4f37738a9752225d1249a45f5904c8
AlbertBui010 <buiquangquy12823@gmail.com>
2025-04-25 08:04:28 +0700

    Finallll

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Banner = () => {
-  return (
-    <div className="relative w-full h-[200px]">
-      <p>toi la heo</p>
-    </div>
-  );
+  return <div className="relative w-full h-[200px]"></div>;
 };
 
 export default Banner;
